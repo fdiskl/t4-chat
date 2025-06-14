20ca502268a5e556522859b21f0771b41384ff96
a <stepanovsm8@gmail.com>
2025-06-14 20:41:21 +0300

    fix

--- src/components/app.tsx
+++ src/components/app.tsx
@@ -3,8 +3,17 @@
 import { BrowserRouter, Route, Routes } from "react-router";
 import Main from "./main";
 import Login from "./login";
+import { useEffect, useState } from "react";
 
 export default function App() {
+  const [isAlreadyClient, setIsAlreadyClient] = useState(false);
+
+  useEffect(() => {
+    setIsAlreadyClient(true);
+  }, []);
+
+  if (!isAlreadyClient) return null;
+
   return (
     <BrowserRouter>
       <Routes>
