2b6cb909a411e4793ccdbc3705c94711ccdc9908
a <stepanovsm8@gmail.com>
2025-06-18 18:51:08 +0300

    origin fix

--- src/app/api/auth/github/login/route.ts
+++ src/app/api/auth/github/login/route.ts
@@ -1,8 +1,12 @@
 import { NextResponse } from "next/server";
 
-export async function GET() {
+export async function GET(req: Request) {
+  const host = req.headers.get("host");
+  const protocol = req.headers.get("x-forwarded-proto") || "http";
+  const origin = `${protocol}://${host}`;
+
   const client_id = process.env.GITHUB_CLIENT_ID!;
-  const redirect_uri = "http://localhost:3000/api/auth/github/callback";
+  const redirect_uri = `${origin}/api/auth/github/callback`;
   const githubURL = `https://github.com/login/oauth/authorize?client_id=${client_id}&redirect_uri=${redirect_uri}&scope=read:user`;
 
   return NextResponse.redirect(githubURL);
