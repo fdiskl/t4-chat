49a5654852a5ecee3011dc867c8bf5b5489eb22a
a <stepanovsm8@gmail.com>
2025-06-18 17:06:34 +0300

    mvp

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,9 +20,9 @@ export const metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en" className={`${inter.variable} ${bricolage.variable}`}>
-      <head>
+      {/* <head>
         <script src="https://unpkg.com/react-scan/dist/auto.global.js" />
-      </head>
+      </head> */}
       <body className="dark font-sans antialiased">
         {children} <Toaster />
       </body>
